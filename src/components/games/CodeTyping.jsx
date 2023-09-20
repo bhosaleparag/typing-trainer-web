ad65322b2403299df6ae191ecc33560186169118
bhosaleparag <paragbhosale06@gmail.com>
2023-09-20 23:43:26 +0530

    commit

--- src/components/games/CodeTyping.jsx
+++ src/components/games/CodeTyping.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState, useRef } from "react";
 
-export default function CodeTyping() {
+const CodeTyping = () => {
+  const activeWord = useRef(null);
+  const caretRef = useRef(null);
+
+  const wordList = ["dsa", "ytrsf","fsdfda", "fsdasf",  "appears", "there", "is", "there", "is","syntax", "issue", "in", "the", "line", "where", "you", "are", "defining", "the" ]
+  const currWord = "appears"
+  const typedWord = "app"
   return (
-    <div>CodeTyping</div>
-  )
-}
+    <div className="test">
+      <div className="box">
+        {wordList.map((word, idx) => {
+          const isActive = currWord === word ;
+          return (
+            <div
+              key={word + idx}
+              className="word"
+              ref={isActive ? activeWord : null}
+            >
+              {isActive ? (
+                <span
+                  ref={caretRef}
+                  id="caret"
+                  className="blink"
+                  style={{
+                    left: typedWord.length * 14.5833,
+                  }}
+                >|</span>
+              ) : null}
+              {word.split("").map((char, charId) => {
+                return <span key={char + charId}>{char}</span>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CodeTyping;
+{/* <input className="wordsInput" tabIndex="0" autoComplete="off" autoCapitalize="off" autoCorrect="off" data-gramm="false" data-gramm_editor="false" data-enable-grammarly="false" list="autocompleteOff" spellCheck="false"
+/> */}
